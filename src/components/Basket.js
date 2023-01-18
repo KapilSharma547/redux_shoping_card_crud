ca432ee15f25593f011869839dc4d34da262acab
Kapil Sharma <kapilsharmanfl97@gmail.com>
2023-01-18 11:47:40 +0530

    function based

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-
 import { util } from "../util";
 import {
   addToCart,
@@ -9,82 +8,74 @@ import {
   decreaseFromCart,
   increaseFromCart,
 } from "../actions/cartActions";
-class Basket extends Component {
-  render() {
-    const { cartItems } = this.props;
 
-    return (
-      <div
-        className="alert alert-info mb-5"
-        style={{ width: "500px", margin: "0 auto" }}
-      >
-        {cartItems.length === 0 ? (
-          "Basket is empty"
-        ) : (
-          <div>
-            You have {cartItems.length} items in the basket. <hr />
-          </div>
-        )}
-        {cartItems.length > 0 && (
-          <div>
-            <ul>
-              {cartItems.map((item) => (
-                <li key={item.id}>
-                  <div className="mb-2">
-                    <b>{item.title}</b>
-                    <button
-                      style={{ float: "right" }}
-                      className="btn btn-danger btn-sm mt-2"
-                      onClick={(e) =>
-                        this.props.removeFromCart(this.props.cartItems, item)
-                      }
-                    >
-                      Remove
-                    </button>
-                  </div>
-                  <span className="mx-2">
-                    {util.formatCurrency(item.price)}
-                  </span>
-                  <button
-                    className="btn btn-info btn-sm ms-5 me-2"
-                    onClick={(e) =>
-                      this.props.decreaseFromCart(this.props.cartItems, item)
-                    }
-                  >
-                    -
-                  </button>
-                  <span>{item.count}</span>
-                  <button
-                    className="btn btn-info btn-sm ms-2"
-                    onClick={(e) =>
-                      this.props.increaseFromCart(this.props.cartItems, item)
-                    }
-                  >
-                    +
-                  </button>
-                  <hr />
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-        <div className="d-flex justify-content-around">
-          {cartItems.length !== 0 && (
-            <b>
-              Sum:{" "}
-              {util.formatCurrency(
-                cartItems.reduce((a, c) => a + c.price * c.count, 0)
-              )}
-            </b>
-          )}
-          <Link to="/" className="btn btn-primary">
-            Go to Products
-          </Link>
+const Basket = (props) => {
+  const { cartItems } = props;
+
+  return (
+    <div
+      className="alert alert-info mb-5"
+      style={{ width: "500px", margin: "0 auto" }}
+    >
+      {cartItems.length === 0 ? (
+        "Basket is empty"
+      ) : (
+        <div>
+          You have {cartItems.length} items in the basket. <hr />
         </div>
+      )}
+      {cartItems.length > 0 && (
+        <div>
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.id}>
+                <div className="mb-2">
+                  <b>{item.title}</b>
+                  <button
+                    style={{ float: "right" }}
+                    className="btn btn-danger btn-sm mt-2"
+                    onClick={(e) => props.removeFromCart(props.cartItems, item)}
+                  >
+                    Remove
+                  </button>
+                </div>
+                <span className="mx-2">{util.formatCurrency(item.price)}</span>
+                <button
+                  className="btn btn-info btn-sm ms-5 me-2"
+                  onClick={(e) => props.decreaseFromCart(props.cartItems, item)}
+                >
+                  -
+                </button>
+                <span>{item.count}</span>
+                <button
+                  className="btn btn-info btn-sm ms-2"
+                  onClick={(e) => props.increaseFromCart(props.cartItems, item)}
+                >
+                  +
+                </button>
+                <hr />
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <div className="d-flex justify-content-around">
+        {cartItems.length !== 0 && (
+          <b>
+            Sum:{" "}
+            {util.formatCurrency(
+              cartItems.reduce((a, c) => a + c.price * c.count, 0)
+            )}
+          </b>
+        )}
+        <Link to="/" className="btn btn-primary">
+          Go to Products
+        </Link>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 const mapStateToProps = (state) => ({
   cartItems: state.cart.items,
 });
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,33 +1,34 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import { connect } from "react-redux";
 import { util } from "../util";
 import { addToCart } from "../actions/cartActions";
 import { fetchProducts } from "../actions/productActions";
-class Products extends Component {
-  componentDidMount() {
-    this.props.fetchProducts();
-  }
-  render() {
-    const productItems = this.props.products.map((product) => (
-      <div className="col-md-4 mb-4" key={product.id}>
-        <div className="bg-light text-center">
-          <img src={`products/${product.sku}_2.jpg`} alt={product.title} />
-          <p>{product.title}</p>
-          <b>{util.formatCurrency(product.price)}</b>
-          <button
-            onClick={(e) => this.props.addToCart(this.props.cartItems, product)}
-            className="btn btn-primary"
-            style={{ cursor: "pointer" }}
-          >
-            Add to cart
-          </button>
-        </div>
-      </div>
-    ));
 
-    return <div className="row">{productItems}</div>;
-  }
-}
+const Products = ({ products, cartItems, fetchProducts, addToCart }) => {
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const productItems = products.map((product) => (
+    <div className="col-md-4 mb-4" key={product.id}>
+      <div className="bg-light text-center">
+        <img src={`products/${product.sku}_2.jpg`} alt={product.title} />
+        <p>{product.title}</p>
+        <b>{util.formatCurrency(product.price)}</b>
+        <button
+          onClick={(e) => addToCart(cartItems, product)}
+          className="btn btn-primary"
+          style={{ cursor: "pointer" }}
+        >
+          Add to cart
+        </button>
+      </div>
+    </div>
+  ));
+
+  return <div className="row">{productItems}</div>;
+};
+
 const mapStateToProps = (state) => ({
   products: state.products.filteredItems,
   cartItems: state.cart.items,
